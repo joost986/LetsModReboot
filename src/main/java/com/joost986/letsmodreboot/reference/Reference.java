2fa55c9abfba50f3c0c0a375ff842500417158c2
joost986 <joost986@hotmail.com>
2014-06-26 19:50:57 +0200

    Test commit

--- src/main/java/com/joost986/letsmodreboot/reference/Reference.java
+++ src/main/java/com/joost986/letsmodreboot/reference/Reference.java
@@ -5,7 +5,6 @@ public class Reference
 	public static final String MOD_ID = "letsmodreboot";
 	public static final String MOD_NAME = "Let's Mod Reboot";
 	public static final String MOD_VERSION = "1.7.2-0.1.1";
-
 	public static final String CLIENT_PROXY_CLASS = "com.joost986.letsmodreboot.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "com.joost986.letsmodreboot.proxy.ServerProxy";
 
