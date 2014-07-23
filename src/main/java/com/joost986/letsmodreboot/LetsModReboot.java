93757eab0cccff22f70a043db064f96ea7801d2f
joost986 <joost986@hotmail.com>
2014-07-23 11:56:22 +0200

    ep13

--- src/main/java/com/joost986/letsmodreboot/LetsModReboot.java
+++ src/main/java/com/joost986/letsmodreboot/LetsModReboot.java
@@ -12,6 +12,7 @@ import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.oredict.OreDictionary;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, guiFactory = Reference.GUI_FACTORY_CLASS)
 public class LetsModReboot
